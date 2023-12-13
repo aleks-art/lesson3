406390e4079df2bea0d758becb30ac1e888f1aa4
aleks <www.lexa.konovko2014@gmail.com>
2023-12-13 22:28:33 +0300

    Задание 2

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте массив на 10 целых чисел. Напишите программу, которая определяет количество чётных чисел в массиве.
+using System;
+using System.IO;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Random rnd = new Random();
+        int count = 0;
+        int[] array = new int [9];
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = rnd.Next(0, 1010);
+
+            if (array[i]%2==0)
+            {
+                count++;
+            }
+        }
+        Console.WriteLine("Количество чётных элементов массива, " + count);
+    }
+}
