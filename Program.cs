f8814a94f10901277aad2db31d5e17f13e3cef29
aleks <www.lexa.konovko2014@gmail.com>
2023-12-13 22:27:11 +0300

    Задание 1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задайте одномерный массив из 10 целых чисел от 1 до 100. Найдите количество элементов массива, значения которых лежат в отрезке [20,90].
+using System;
+using System.IO;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Random rnd = new Random();
+        int count = 0;
+        int[] array = new int [9];
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = rnd.Next(0, 101);
+
+            if (array[i] > 20 && array[i] < 90)
+            {
+                count++;
+            }
+        }
+        Console.WriteLine("Количество элементов массива, значения которых лежат в отрезке [20,90] " + count);
+    }
+}
